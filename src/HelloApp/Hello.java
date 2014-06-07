25a2e620aa3d0d5073e740d2f139adc327ca28f0
jurin <jurin.nakano@gmail.com>
2014-06-07 15:53:03 +0900

    first commit

---
+++ src/HelloApp/Hello.java
@@ -0,0 +1,13 @@
+package HelloApp;
+
+
+/**
+* HelloApp/Hello.java .
+* IDL-to-Javaコンパイラ(ポータブル)、バージョン"3.2"によって生成されました
+* Hello.idlから
+* 2014年5月19日 20時59分57秒 JST
+*/
+
+public interface Hello extends HelloOperations, org.omg.CORBA.Object, org.omg.CORBA.portable.IDLEntity 
+{
+} // interface Hello
---
+++ src/HelloApp/HelloOperations.java
@@ -0,0 +1,15 @@
+package HelloApp;
+
+
+/**
+* HelloApp/HelloOperations.java .
+* IDL-to-Javaコンパイラ(ポータブル)、バージョン"3.2"によって生成されました
+* Hello.idlから
+* 2014年5月19日 20時59分57秒 JST
+*/
+
+public interface HelloOperations 
+{
+  String sayHello ();
+  void shutdown ();
+} // interface HelloOperations
